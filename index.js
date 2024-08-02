dd7ab2d3f33e5d7378a58b77c03075b8402b6562
Arsen Stakhiv <arsen.stakhiv2305@gmail.com>
2024-08-02 13:32:13 +0300

    add: add score variables add: add playRound function add: add helper didHumanWin function add: add return to getHumanChoice function add: add 1 round game

--- index.js
+++ index.js
@@ -27,4 +27,39 @@ function getHumanChoice() {
     }
 
     console.log(choice);
+    return choice;
 }
+
+function didHumanWin(human, computer) {
+    if (human === "rock" && computer === "scissors") {
+        return true;
+    } else if (human === "paper" && computer === "rock") {
+        return true;
+    } else if (human === "scissors" && computer === "paper") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It is a tie");
+    } else if (didHumanWin(humanChoice, computerChoice)){
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+
+    console.log(`Score: ${humanScore}:${computerScore}`);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
